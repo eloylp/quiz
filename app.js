a68f19509ac8de4fd02e0309474c48d714ea058d
lute <lute@phobos>
2015-06-28 22:17:06 +0200

    more bugs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(viewPartials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser('Quiz 2015'));
 app.use(session({
                  secure: true,
